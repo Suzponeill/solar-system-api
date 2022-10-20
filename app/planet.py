e23356eb61c936ad434c62343e5a908261ce3dd0
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-10-20 11:41:28 -0700

    wave1 debugging

--- app/planet.py
+++ app/planet.py
@@ -1,6 +1,8 @@
+from flask import Blueprint, jsonify
+
 class Planet:
     def __init__(self,id = None, name = None,description = None, moons = None):
-        self.id - id
+        self.id = id
         self.name = name
         self.description = description
         if self.moons is None:
@@ -14,3 +16,18 @@ mars = Planet('Mars','A smaller red planet')
 jupiter = Planet('Jupiter', 'A gas giant',['Europa','Ganymede', 'Calysto'])
 
 planets_list = [home, mars, jupiter]
+
+planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planet")
+
+@planet_bp.route("", methods = ["GET"])
+def get_all_planets():
+    planets = []
+    for planet in planets_list:
+        planet_dict = {
+            "id": planet.id,
+            "name": planet.name,
+            "description":planet.description,
+            "moons":planet.moons
+        }
+        planets.append(planet_dict)
+    return jsonify(planets)
