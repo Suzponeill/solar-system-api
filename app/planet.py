35af4d2ec03a530ed7c0c227794adac8357d4f34
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-10-21 11:26:16 -0700

    Wave 2: Create endpoint for 1 planet with error handling

--- app/planet.py
+++ app/planet.py
@@ -1,22 +1,22 @@
 from flask import Blueprint, jsonify
 
 class Planet:
-    def __init__(self,id = None, name = None,description = None, moons = None):
+    def __init__(self,id, name, description, moons = None):
         self.id = id
         self.name = name
         self.description = description
-        if self.moons is None:
+        if moons is None:
             self.moons = []
         else:
             self.moons = moons
 
 
-home = Planet('Earth','A pale blue dot.',["earth's moon"])
-mars = Planet('Mars','A smaller red planet')
-jupiter = Planet('Jupiter', 'A gas giant',['Europa','Ganymede', 'Calysto'])
+home = Planet(4, 'Earth','A pale blue dot.',["earth's moon"])
+mars = Planet(3, 'Mars','A smaller red planet')
+jupiter = Planet(5, 'Jupiter', 'A gas giant',['Europa','Ganymede', 'Calysto'])
+
 
 planets_list = [home, mars, jupiter]
-
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planet")
 
 @planet_bp.route("", methods = ["GET"])
@@ -31,3 +31,21 @@ def get_all_planets():
         }
         planets.append(planet_dict)
     return jsonify(planets)
+
+@planet_bp.route("/<planet_id>", methods = ["GET"])
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return {"message": "planet_id must be an integer"},400
+    
+    for planet in planets_list:
+        if planet_id == planet.id:
+            return {
+            "id": planet.id,
+            "name": planet.name,
+            "description":planet.description,
+            "moons":planet.moons
+        },200
+
+    return {"message": f"{planet_id} not found in planets list."}, 404
